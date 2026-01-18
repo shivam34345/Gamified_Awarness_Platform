192af1631aa96c8b3d38c34614e3b4bbb96ce6b3
DARSHIT SHAH <166908115+DARSHITSHAH-2906@users.noreply.github.com>
2026-01-18 22:39:50 +0530

    updated the cookie generation

--- backend/src/services/generateToken.ts
+++ backend/src/services/generateToken.ts
@@ -5,7 +5,7 @@ export const generateToken = (payload: { id: any }, res: any) => {
     const token = jwt.sign(payload, process.env.JWT_SECRET as string, { expiresIn: '1d' })
     res.cookie('token', token, {
         httpOnly: false,
-        sameSite: false,
+        sameSite: "none",
         secure: process.env.NODE_ENV === 'production',
         maxAge: 24 * 60 * 60 * 1000,
     })
